197e19df0823ef54629c80d1c04fc4693c2bc988
Mauricio Pocas <mbpocas@hotmail.com>
2022-08-21 17:36:59 -0300

    OK

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  CATEGORIES = %w[chinese italian japanese french belgian].freeze
+  CATEGORIES = %w[Belgian Chinese French Italian Japanese].freeze
   validates :name, :address, presence: true
   validates :category, inclusion: { in: CATEGORIES }
   has_many :reviews, dependent: :destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Restaurant.destroy_all if Rails.env.development?
+Restaurant.destroy_all
 
 puts 'Creating restaurants...'
 Restaurant.create!(
